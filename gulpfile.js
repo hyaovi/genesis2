cc8c4d391324d5f3ec6ef96463d5e1e4993595f0
Houngue Yaovi <hfyaovi@gmail.com>
2020-05-21 12:21:56 +0300

    gulpfile: add directory constants, better structure of watchers functions. package.json: add npm script:"start"

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,25 @@ const cssAutoprefixer = require('gulp-autoprefixer');
 sass.compiler = require('node-sass');
 
 // path definition
+const [DEV_BASE_DIR, PROD_BASE_DIR] = ['./src', './prod'];
 const paths = {
   src: {
-    home: './src',
-    html: './src/**/*.html',
-    sass: './src/scss/**/*.scss',
-    js: './src/js/**/*.js',
-    cssFiles: './src/css/**/*.css',
-    css: './src/css',
+    home: `${DEV_BASE_DIR}`,
+    html: `${DEV_BASE_DIR}/**/*.html`,
+    sass: `${DEV_BASE_DIR}/scss/**/*.scss`,
+    js: `${DEV_BASE_DIR}/js/**/*.js`,
+    jsFiles: `${DEV_BASE_DIR}/js/**/*.js`,
+    css: `${DEV_BASE_DIR}/css`,
+    cssFiles: `${DEV_BASE_DIR}/css/**/*.css`,
+  },
+  dest: {
+    home: `${PROD_BASE_DIR}`,
+    css: `${PROD_BASE_DIR}/css`,
+    js: `${PROD_BASE_DIR}/js/`,
   },
 };
+
 function compileSass(cb) {
-  console.log('compilling sass');
   src(paths.src.sass)
     .pipe(sass().on('error', sass.logError))
     .pipe(cssAutoprefixer({ cascade: false }))
@@ -26,6 +33,7 @@ function compileSass(cb) {
     .pipe(browserSync.stream());
   cb();
 }
+
 function minifyCss(cb) {
   src(paths.src.cssFiles)
     .pipe(cleanCss({ compatibility: 'ie8' }))
@@ -33,49 +41,37 @@ function minifyCss(cb) {
   cb();
 }
 
-function defaultTask(cb) {
-  console.log('gulp default !!!');
+const browserSyncReload = (cb) => {
+  browserSync.reload();
   cb();
-}
+};
 
 // WATCHERS
 function watchSass() {
-  watch(
-    paths.src.sass,
-    series(compileSass, (cb) => {
-      browserSync.reload();
-      cb();
-    })
-  );
+  watch(paths.src.sass, compileSass);
 }
+
 function watchHtml() {
-  watch(paths.src.html, (cb) => {
-    browserSync.reload();
-    cb();
-  });
+  watch(paths.src.html, browserSyncReload);
 }
 function watchJS() {
-  watch(paths.src.js, (cb) => {
-    browserSync.reload();
-    cb();
-  });
+  watch(paths.src.js, browserSyncReload);
 }
 
 // LAUNCH SERVER
-function staticServer(cb) {
+function staticServer() {
   browserSync.init({
     server: {
       baseDir: './src/',
     },
   });
-  cb();
 }
 
 // EXPORTS
 exports.css = minifyCss;
 exports.sass = watchSass;
-exports.default = function (cb) {
-  staticServer(cb);
+exports.default = function startGulpDefault() {
+  staticServer();
   watchSass();
   watchHtml();
   watchJS();
